687bea67be678c71deb42a982255d3fd4bcd63e3
softstream-link <softstream-link@users.noreply.github.com>
2023-07-08 09:59:49 -0400

    Error trait

--- byteserde/src/error.rs
+++ byteserde/src/error.rs
@@ -1,12 +1,24 @@
-use std::fmt;
+use std::fmt::{self, Display};
 
 pub struct SerDesError {
     pub message: String,
 }
 
+impl std::error::Error for SerDesError {} 
+
 impl fmt::Debug for SerDesError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str(format!("SerDesError {{\n\tmessage: {}\n}}", self.message).as_str())
+        f.debug_struct("SerDesError")
+            .field("message", &self.message)
+            .finish()
+        // f.write_str(format!("SerDesError {{\n\tmessage: {}\n}}", self.message).as_str())
+    }
+}
+impl Display for SerDesError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("SerDesError")
+            .field("message", &self.message)
+            .finish()
     }
 }
 
